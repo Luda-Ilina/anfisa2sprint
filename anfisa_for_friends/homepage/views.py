84ea9050652d0f5f4b3579f738ed9e9215d1d686
Liudmila Ilina <liudmilla.ilina@gmail.com>
2024-08-21 15:53:15 +0200

    Added fields Price and Output_order in IceCream Model

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,32 @@
+from django.db.models import Q
 from django.shortcuts import render
+from ice_cream.models import Category, IceCream
 
 
 def index(request):
-    template = 'homepage/index.html'
-    return render(request, template)
+    template_name = 'homepage/index.html'
+    # Запрос:
+    ice_cream_list = IceCream.objects.values(
+        'id', 'title', 'description'
+    ).filter(
+        (Q(is_published=True) & Q(is_on_main=True))
+        | (Q(title__contains='эскимо') & Q(is_published=True))
+    ).order_by('title')[1:4]
+
+    # categories = Category.objects.values(
+    #     'id', 'output_order', 'title'
+    # ).order_by(
+    #     # Сортируем записи по значению поля output_order,
+    #     # а если значения output_order у каких-то записей равны -
+    #     # сортируем эти записи по названию в алфавитном порядке.
+    #     'output_order', 'title'
+    # )
+
+    # Полученный из БД QuerySet передаём в словарь контекста:
+    context = {
+        'ice_cream_list': ice_cream_list,
+        # 'categories': categories
+    }
+
+    # Словарь контекста передаём в шаблон, рендерим HTML-страницу:
+    return render(request, template_name, context)
--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,54 @@
+# ice_cream/admin.py
 from django.contrib import admin
 
-# Register your models here.
+# Из модуля models импортируем модель Category...
+from .models import Category, IceCream, Topping, Wrapper
+
+admin.site.empty_value_display = 'Не задано in app'
+
+
+class IceCreamInline(admin.StackedInline):
+    model = IceCream
+    extra = 0
+    filter_horizontal = ('toppings',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (
+        IceCreamInline,
+    )
+    list_display = (
+        'title',
+    )
+
+
+class IceCreamAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category',
+    )
+    search_fields = ('title',)
+    list_filter = ('category',)
+    list_display_links = ('title',)
+    empty_value_display = 'Не задано'
+    filter_horizontal = ('toppings',)
+
+
+
+
+
+# ...и регистрируем её в админке:
+admin.site.register(IceCream, IceCreamAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Topping)
+admin.site.register(Wrapper)
+
