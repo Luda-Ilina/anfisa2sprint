21babeff7a148c2ff68709ae15f17451764b1cc0
Liudmila Ilina <liudmilla.ilina@gmail.com>
2024-08-21 16:09:58 +0200

    Added ordering in class Meta of IceCream Model

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -6,7 +6,7 @@ from ice_cream.models import IceCream  # Category
 def index(request):
     template_name = 'homepage/index.html'
     ice_cream_list = IceCream.objects.values(
-        'id', 'title', 'description', 'price'
+        'id', 'title', 'price',  'description'
     ).filter(
         is_published=True,
         is_on_main=True,
